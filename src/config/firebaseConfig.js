e277cd5fbdee2ad323add3dd990fea985a098a2c
Yudawardana94 <yuda.wardana40@gmail.com>
2022-03-10 12:21:45 +0700

    implement firebase on Android

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,25 +1,36 @@
 import firebase from '@react-native-firebase/app';
-import '@react-native-firebase/database';
+import database from '@react-native-firebase/database';
 import firestore from '@react-native-firebase/firestore';
 
-const defaultDB = 'https://ohmyproject-2020.firebaseio.com/';
+// const defaultDB = 'https://ohmyproject-2020.firebaseio.com/';
 
 export const readDB = param => {
   console.log(param, 'ini param');
-  firebase
-    .app()
-    .database()
-    .ref('/userData/00')
-    .once('value', snapshot => console.log(snapshot, ' ini snapshot'))
-    .catch(error => console.log(error, 'ini error'));
+  // firebase
+  //   .app()
+  //   .database()
+  //   .ref('/userData/00')
+  //   .once('value', snapshot => console.log(snapshot, ' ini snapshot'))
+  //   .catch(error => console.log(error, 'ini error'));
+  // database()
+  //   .ref('/users/123')
+  //   .set('hello')
+  //   .then(() => console.log('Data set.'))
+  // database()
+  //   .ref('/users/123')
+  //   .once('value')
+  //   .then(snapshot => {
+  //     console.log(snapshot);
+  //     console.log('User data: ', snapshot.val());
+  //   })
+  //   .catch(e => console.log('Data Error', e));
 };
 
 export const readFSDB = async param => {
   console.log(param, 'ini paramnya');
   try {
-    const restaurant = await firestore().collection(param).get();
-    // console.log(restaurant._docs);
-    return restaurant._docs;
+    const resultList = await firestore().collection(param).get();
+    return resultList._docs;
   } catch (error) {
     console.log(error, 'ada error disini');
   }
